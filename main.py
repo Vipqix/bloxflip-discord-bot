e68800c0e7a41bc57bfcb0f5ff4a29b960a98d95
Vipqix <101670879+Vipqix@users.noreply.github.com>
2023-01-23 16:28:51 +0100

    Update main.py
    finally the towers thing

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from discord import app_commands
 import discord
 import json
 import requests
-from discord import app_commands
+import random
 
 
 class aclient(discord.Client):
@@ -20,6 +21,7 @@ client = aclient()
 tree = app_commands.CommandTree(client)
 
 headersnoauth = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
 
 with open("database.json", "r") as f:
     tokens = json.load(f)
@@ -40,11 +42,11 @@ async def link(interaction: discord.Interaction, auth: str):
 
             await interaction.edit_original_response(content='', embed=discord.Embed(title='Linked!', description="you've linked your bloxflip account to our system and can now use the bot", color=0x1aff00))
 
-            with open("database.json", "r") as f:
-                tokens = json.load(f)
-            tokens[str(interaction.user.id)] = auth
-            with open("database.json", "w") as f:
-                json.dump(tokens, f)
+            with open("database.json", "r") as ff:
+                tokenss = json.load(ff)
+            tokenss[str(interaction.user.id)] = auth
+            with open("database.json", "w") as ff:
+                json.dump(tokenss, ff)
         else:
             await interaction.edit_original_response(content='', embed=discord.Embed(title='Error!', description="Not a valid token.", color=0xff0000))
 
@@ -59,6 +61,97 @@ async def sendquote(interaction: discord.Interaction):
         await interaction.response.send_message(embed=discord.Embed(title="Your auth", description=f"```{auth}```", color=0x1aff00), ephemeral=True)
     except KeyError:
         await interaction.response.send_message("You have not linked your auth!")
-        
 
-client.run(open("run", "r").read())
+@tree.command(name='privuser', description='gives info about a user')
+async def privuser(interaction: discord.Interaction):
+    if str(interaction.user.id) not in tokens:
+        await interaction.response.send_message(embed=discord.Embed(title="Error!", description="You have not linked your token yet, use `/link` to link your account :)"), ephemeral=True)
+        return
+
+    await interaction.response.send_message(embed=discord.Embed(title="Getting user info...", description="Please wait while we get your info...", color=0xffff00), ephemeral=True)
+    aheader = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+        'x-auth-token': json.load(open("database.json", "r"))[str(interaction.user.id)]
+    }
+    request = requests.get(url="https://api.bloxflip.com/user", headers=aheader).json()
+    await interaction.edit_original_response(content='', embed=discord.Embed(title="User info", description=f"```json\n{json.dumps(request, indent=4)}```", color=0x1aff00))
+
+@tree.command(name='pubuser', description='gives info about a user')
+async def pubuser(interaction: discord.Interaction):
+    if str(interaction.user.id) not in tokens:
+        await interaction.response.send_message(embed=discord.Embed(title="Error!", description="You have not linked your token yet, use `/link` to link your account :)"), ephemeral=True)
+        return
+
+    await interaction.response.send_message(embed=discord.Embed(title="Getting user info...", description="Please wait while we get your info...", color=0xffff00), ephemeral=False)
+    aheader = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+        'x-auth-token': json.load(open("database.json", "r"))[str(interaction.user.id)]
+    }
+    request = requests.get(url="https://api.bloxflip.com/user", headers=aheader).json()['user']
+    robloxID = request['robloxId']
+    robloxNAME = request["robloxUsername"]
+    wallet = request['wallet']
+    embed = discord.Embed(title=f"User info from {interaction.user}", color=0x1aff00)
+    embed.add_field(name="Roblox ID", value=robloxID)
+    embed.add_field(name="Roblox Username", value=robloxNAME)
+    embed.add_field(name="Wallet", value=wallet)
+    await interaction.edit_original_response(content='', embed=embed)
+
+@tree.command(name='autotowers', description='plays a towers game for you')
+async def autotowers(interaction: discord.Interaction, bet_amount: int, difficulty: str, clicks: int):
+
+    if str(interaction.user.id) not in tokens:
+        await interaction.response.send_message(embed=discord.Embed(title="Error!", description="You have not linked your token yet, use `/link` to link your account :)"), ephemeral=True)
+        return
+
+    if bet_amount < 5:
+        await interaction.response.send_message(embed=discord.Embed(title="Error!", description="Bet amount must be greater than 5", color=0xff0000), ephemeral=True)
+        return
+
+    if difficulty.lower() not in ['easy', 'medium', 'hard']:
+        await interaction.response.send_message(embed=discord.Embed(title="Error!", description="Your difficulty is not [easy, normal, hard].", color=0xff0000), ephemeral=True)
+        return
+
+    if clicks < 1:
+        await interaction.response.send_message(embed=discord.Embed(title="Error!", description="You must click at least once.", color=0xff0000), ephemeral=True)
+        return
+
+    await interaction.response.send_message(embed=discord.Embed(title="Starting...", description="We're starting a game for you, good luck.", color=0xffff00))
+
+    aheader = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+        'x-auth-token': json.load(open("database.json", "r"))[str(interaction.user.id)]
+    }
+    savedwallet = requests.get(url="https://api.bloxflip.com/user", headers=aheader).json()['user']['wallet']
+    try:
+        """ creates a game """
+        requests.post(url="https://api.bloxflip.com/games/towers/create", headers=aheader, json={"difficulty":difficulty,"betAmount":bet_amount}).json()
+
+        """" makes clicks """
+        towers_stage = 0
+        await interaction.edit_original_response(content='', embed=discord.Embed(title="Clicking...", color=0xffff00))
+        for i in range(clicks):
+            play_request = requests.post(url="https://api.bloxflip.com/games/towers/action", headers=aheader,
+                                         json={"cashout": False, "tile": random.randint(0, 2), "towerLevel": towers_stage}).json()
+            if play_request['exploded']:
+                embed = discord.Embed(title="Game ended!", description=f"Game ended, you lost.", color=0xff0000)
+                wallet = requests.get(url="https://api.bloxflip.com/user", headers=aheader).json()['user']['wallet']
+                embed.add_field(name='balance:', value=f'```{wallet}```')
+                embed.add_field(name='loss:', value=f'```{savedwallet - wallet}```')
+                await interaction.edit_original_response(content='', embed=embed)
+                return
+
+        """ tells you if the game is over """
+        requests.post(url="https://api.bloxflip.com/games/towers/action", headers=aheader, json={"cashout": True}).json()
+        embed = discord.Embed(title="Game ended!", description=f"Game ended, you won.", color=0x1aff00)
+        wallet = requests.get(url="https://api.bloxflip.com/user", headers=aheader).json()['user']['wallet']
+        embed.add_field(name='balance:', value=f'```{wallet}```')
+        embed.add_field(name='profit:', value=f'```{wallet - savedwallet}```')
+        await interaction.edit_original_response(content='', embed=embed)
+
+
+    except Exception as e:
+        await interaction.edit_original_response(content='', embed=discord.Embed(title="Error", description=f"Token invalid!, or this error : {e}", color=0xff0000))
+
+
+client.run(open("run", 'r').read())
